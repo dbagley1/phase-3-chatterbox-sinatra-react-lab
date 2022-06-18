468b2317a28ca069f575432b057ca067724cdfd1
Darrian Bagley <dbagle14@gmail.com>
2022-06-18 14:17:43 -0400

    Completed assignment

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,24 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+
   # Add routes
-  
+  get '/messages' do
+    Message.order(created_at: :asc).to_json
+  end
+
+  post '/messages' do
+    Message.create(params).to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(params)
+    message.to_json
+  end
+
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+  end
 end
